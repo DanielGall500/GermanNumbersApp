740a42872aaabebd0478e0d73f8c47dc6736d8b7
Daniel Gallagher <danielgallagher500@gmail.com>
2020-06-03 17:30:13 +0100

    Fixed Turns Handler

--- app/src/main/java/com/example/germanmemoriserapp/Game.java
+++ app/src/main/java/com/example/germanmemoriserapp/Game.java
@@ -1,29 +1,63 @@
 package com.example.germanmemoriserapp;
 
+import java.util.ArrayList;
+
 public class Game {
 
-    NumSupplier generator = new NumSupplier();
+    private NumSupplier generator;
+    private Score playerScore = new Score();
 
     private int currentNumber;
 
     private int TURN = 0;
+    private int NUM_TURNS;
 
-    public Game() {}
-
-    public int getNextNumber()
+    public Game(int turns)
     {
-        currentNumber = generator.next();
-        System.out.println(currentNumber);
-        return generator.next();
+        this.NUM_TURNS = turns;
+
+        generator = new NumSupplier(1, 10, NUM_TURNS);
+
+        newTurn();
     }
 
-    public int getCurrentNumber()
+    public int getNumber()
     {
         return currentNumber;
     }
 
-    public void enterInput(int input)
+    public int getScore()
     {
-        //do something with score
+        return playerScore.getScore();
     }
+
+    public void parse(int input)
+    {
+        updateScore(input);
+    }
+
+    private void updateScore(int input)
+    {
+        if(generator.isCorrectNumber(input))
+            playerScore.increment();
+    }
+
+    public boolean isEndOfGame()
+    {
+        return (TURN == NUM_TURNS);
+    }
+
+    public void newTurn()
+    {
+        if(isEndOfGame())
+        {
+            throw new RuntimeException("EOG Reached");
+        }
+        else
+        {
+            TURN++;
+            currentNumber = generator.next();
+        }
+    }
+
 }
