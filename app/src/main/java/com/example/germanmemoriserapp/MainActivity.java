30c198245190c17f699300bf9877751c1788278c
Daniel Gallagher <danielgallagher500@gmail.com>
2020-06-03 17:30:50 +0100

    Score View Added

--- app/src/main/java/com/example/germanmemoriserapp/MainActivity.java
+++ app/src/main/java/com/example/germanmemoriserapp/MainActivity.java
@@ -6,36 +6,62 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private String enterNumberTxt = "Got it!";
+    private final String emptyTxt = "";
+
+    private final int TOTAL_TURNS = 3;
 
     EditText enterNumberBox;
     Button enterButton;
+    TextView tmpNumberView;
+    EditText scoreView;
 
-    Game GAME = new Game();
+    Game GAME;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        enterNumberBox = findViewById(R.id.enterNumberBox);
+        GAME = new Game(TOTAL_TURNS);
 
+        enterNumberBox = findViewById(R.id.enterNumberBox);
+        tmpNumberView = findViewById(R.id.tmpNumberView);
         enterButton = findViewById(R.id.enterButton);
+        scoreView = findViewById(R.id.scoreView);
+
+        tmpNumberView.setText(String.valueOf(GAME.getNumber()));
+        scoreView.setText(String.valueOf(GAME.getScore()));
         enterButton.setText(enterNumberTxt);
 
         enterButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v) {
+                //Parse input
                 String strInput = enterNumberBox.getText().toString();
-
                 int INPUT = Integer.parseInt(strInput);
 
-                GAME.enterInput(INPUT);
-                enterNumberBox.setText("");
+                GAME.parse(INPUT);
+
+                //Change Text Views
+                if(!GAME.isEndOfGame())
+                {
+                    GAME.newTurn();
+
+                    enterNumberBox.setText(emptyTxt);
+                    tmpNumberView.setText(String.valueOf(GAME.getNumber()));
+                    scoreView.setText(String.valueOf(GAME.getScore()));
+                }
+                else
+                {
+                    scoreView.setText(String.valueOf(GAME.getScore()));
+                    tmpNumberView.setText("Game Over !");
+                }
             }
         });
 
