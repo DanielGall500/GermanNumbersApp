6f1740f0ab3b6d40d8f2dd18bc0146ca9fdcbc07
Daniel Gallagher <danielgallagher500@gmail.com>
2020-06-10 14:42:36 +0100

    moves from curr activity

--- app/src/main/java/com/example/germanmemoriserapp/listeners/MoveToNewActivityListener.java
+++ app/src/main/java/com/example/germanmemoriserapp/listeners/MoveToNewActivityListener.java
@@ -1,4 +1,32 @@
 package com.example.germanmemoriserapp.listeners;
 
-public class MoveToNewActivityListener {
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MoveToNewActivityListener implements View.OnClickListener {
+
+    Context currentContext;
+    AppCompatActivity currentActivity;
+
+    Intent moveToNewScreen;
+
+    public MoveToNewActivityListener(Context currContext,
+                                     AppCompatActivity currActivity,
+                                     Class newActivity)
+    {
+        this.currentContext = currContext;
+        this.currentActivity = currActivity;
+
+        moveToNewScreen = new Intent(currActivity, newActivity);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        currentContext.startActivity(moveToNewScreen);
+        currentActivity.finish();
+    }
 }
