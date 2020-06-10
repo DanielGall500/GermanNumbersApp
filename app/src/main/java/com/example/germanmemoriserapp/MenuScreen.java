b76fad0a62e7c9fa6117c14e429a9326c74d49e6
Daniel Gallagher <danielgallagher500@gmail.com>
2020-06-10 14:44:16 +0100

    ui improvements

--- app/src/main/java/com/example/germanmemoriserapp/MenuScreen.java
+++ app/src/main/java/com/example/germanmemoriserapp/MenuScreen.java
@@ -2,13 +2,29 @@ package com.example.germanmemoriserapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.germanmemoriserapp.listeners.ExitAppListener;
+import com.example.germanmemoriserapp.listeners.MoveToNewActivityListener;
 
 public class MenuScreen extends AppCompatActivity {
+
+    Button playGameBtn, exitGameBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_screen);
+
+        playGameBtn = findViewById(R.id.playGameBtn);
+        exitGameBtn = findViewById(R.id.exitGameBtn);
+
+        playGameBtn.setOnClickListener(new MoveToNewActivityListener(
+                this,this, MainActivity.class));
+
+        exitGameBtn.setOnClickListener(new ExitAppListener(this));
     }
 }
