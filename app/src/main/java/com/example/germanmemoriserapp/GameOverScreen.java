c4fd125bf674bf7277d7c98a1e298f3ee1b51897
Daniel Gallagher <danielgallagher500@gmail.com>
2020-06-10 14:43:28 +0100

    listener package used for exit button

--- app/src/main/java/com/example/germanmemoriserapp/GameOverScreen.java
+++ app/src/main/java/com/example/germanmemoriserapp/GameOverScreen.java
@@ -1,64 +1,68 @@
 package com.example.germanmemoriserapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.germanmemoriserapp.listeners.MoveToNewActivityListener;
+
 public class GameOverScreen extends AppCompatActivity {
 
-    Intent moveToNewGame, moveToMenu, moveFromGame;
-
-    Button GO_backToMenu, GO_retry;
-    TextView scoreResultView;
-
-    private String backToMenuTxt = "MENU";
-    private String retryTxt = "RETRY";
-
-    String gameScore;
+    private Button retryBtn;
+    private Button backToMenuBtn;
+    private TextView scoreResultView;
+    private String gameScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over_screen);
 
-        moveToNewGame = new Intent(GameOverScreen.this, MainActivity.class);
-        moveToMenu = new Intent(GameOverScreen.this, MenuScreen.class);
-
-        moveFromGame = getIntent();
-        gameScore = moveFromGame.getStringExtra(getString(R.string.score_key));
-
-        GO_backToMenu = findViewById(R.id.GO_backToMenu);
-        GO_retry = findViewById(R.id.GO_retry);
+        retryBtn = findViewById(R.id.GO_retry);
+        backToMenuBtn = findViewById(R.id.GO_backToMenu);
         scoreResultView = findViewById(R.id.GO_scoreResultsView);
 
-        GO_backToMenu.setText(backToMenuTxt);
-        GO_retry.setText(retryTxt);
-        scoreResultView.setText(getScoreResult());
+        String scoreKey = getString(R.string.score_key);
+        String score = retrieveScoreFromGame(scoreKey);
 
-        GO_backToMenu.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-                startActivity(moveToMenu);
-            }
-        });
+        setScoreString(getScoreString(score));
 
-        GO_retry.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v) {
-                startActivity(moveToNewGame);
-            }
-        });
+        backToMenuBtn.setOnClickListener(new MoveToNewActivityListener(
+                this, this, MenuScreen.class
+        ));
 
+        retryBtn.setOnClickListener(new MoveToNewActivityListener(
+                this, this, MainActivity.class
+        ));
     }
 
-    private String getScoreResult()
-    {
-        return String.format("Your Score Is %s seconds!", gameScore);
+    private TextView getScoreView() {
+        return this.scoreResultView;
     }
+
+    private void setScoreString(String result) {
+        getScoreView().setText(result);
+    }
+
+    public String getScoreString(String score) {
+        return String.format("Your Score Is %s seconds!", score);
+    }
+
+    private String retrieveScoreFromGame(String key) {
+        Intent moveFromGame = getIntent();
+        return moveFromGame.getStringExtra(key);
+    }
+
+    public String getScore() {
+        return gameScore;
+    }
+
+    private void setScore(String score) {
+        gameScore = score;
+    }
+
+
 }
