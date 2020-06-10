b2a379c0bcd0aed3c28ff1ac0642a9ab7be821b3
Daniel Gallagher <danielgallagher500@gmail.com>
2020-06-10 14:41:44 +0100

    template for changing activity

--- app/src/main/java/com/example/germanmemoriserapp/listeners/ExitAppListener.java
+++ app/src/main/java/com/example/germanmemoriserapp/listeners/ExitAppListener.java
@@ -1,4 +1,29 @@
 package com.example.germanmemoriserapp.listeners;
 
-public class ExitAppListener {
+import android.view.View;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ExitAppListener implements View.OnClickListener  {
+
+    AppCompatActivity currentActivity;
+
+    public ExitAppListener(AppCompatActivity act) {
+        this.currentActivity = act;
+    }
+
+    @Override
+    public void onClick(View v) {
+        exitApp();
+    }
+
+    private void exitApp() {
+        currentActivity.finish();
+        System.exit(0);
+    }
+
 }
