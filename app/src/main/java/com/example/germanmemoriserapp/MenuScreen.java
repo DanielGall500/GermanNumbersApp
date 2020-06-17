44bbef94bf1fb7ce437946ae2363879f2ba8374f
Daniel Gallagher <danielgallagher500@gmail.com>
2020-06-17 16:30:27 +0100

    minor improvements

--- app/src/main/java/com/example/germanmemoriserapp/MenuScreen.java
+++ app/src/main/java/com/example/germanmemoriserapp/MenuScreen.java
@@ -1,11 +1,7 @@
 package com.example.germanmemoriserapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
 
